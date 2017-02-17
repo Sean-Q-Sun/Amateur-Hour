c9f7011d338778550ee61017f0fbe27d3e0ade44
Sean Sun <seansun.q@gmail.com>
2017-02-16 23:05:34 -0500

    FINISHED

--- httpRequests.py
+++ httpRequests.py
@@ -2,72 +2,116 @@
 import socket
 import urllib
 
+
+""" Connect to the host on the port
+"""
 def connection(host, port):
     s = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)
     s.connect((host, port))
     return s
 
-"""Get the flag by doing a POST request
-"""
-def getFlag(host, port):
-    flag = ""
+def getItem(s):
+    item = "";
+    while True:
+        response = s.recv(1024)
+        if response:
+            item = response
+        break
+    item = item.split()[len(item.split())-1][:-1]
+    return item 
+
+def getFlag1(host, port):
     request = "POST / HTTP/1.1\n"
-    reqHost = "Host: 54.209.150.110\n\n"
-    s = connection(host,port)
-    s.send(request + reqHost)
-    while True:
-        response = s.recv(1024)
-        if response: 
-            flag = response
-        break
-    flag = flag.split()[len(flag.split())-1][:-1]
-    s.close()
-    return flag
-
-def getToken(host, port):
-    token = ""
-    request = "POST /getSecure HTTP/1.1\n"
-    reqHost = "Host: 54.209.150.110\n\n"
-    cLife = "Connection: keep-alive\n\n"
-    s = connection(host,port)
-    s.send(request + reqHost + cLife)
-    while True:
-        response = s.recv(1024)
-        if response: 
-            token = response
-        break
-    token = token.split()[len(token.split())-1][:-1]
-    s.close()
-    return token
-
-def getFlag2(host, port, token):
-    flag = ""
-    request = "POST /getFlag2 HTTP/1.1\n"
     reqHost = "Host: "+host+"\n\n"
-    cLife = "Connection: keep-alive\n\n" 
+    s = connection (host, port)
+    s.send(request + reqHost)
+    return getItem(s)
+
+def getFlag2(host, port):
+    request = "POST /getSecure HTTP/1.1\n"
+    reqHost = "Host: "+host+"\n"
+    s = connection(host, port)
+    s.send(request + reqHost +"Connection: keep-alive\n\n")
+    token = getItem(s)
+    s.close()
+
+    request = "POST /getFlag2 HTTP/1.1\n"
+    s = connection(host, port)
+
     token = "token="+token+"\n\n"
     cType = "Content-Type: application/x-www-form-urlencoded\n"
     cLength = "Content-Length: {}\n\n".format(len(token)-2)
+    s.send(request + reqHost + cType + cLength + token)
+    return getItem(s)
+
+def getFlag3(host, port):
+    request = "POST /getSecure HTTP/1.1\n"
+    reqHost = "Host: "+host+"\n"
     s = connection(host,port)
-    s.send(request + reqHost + cType + cLength + cLife + token)
-    while True:
-        response = s.recv(1024)
-        print response
-        if response:
-            flag = response
-        break
-    flag = flag.split()[len(flag.split())-1][:1]
+    s.send(request + reqHost + "Connection: keep-alive\n\n")
+    token = getItem(s)
     s.close()
-    return flag
+
+    request = "POST /getFlag3Challenge HTTP/1.1\n"
+    reqHost = "Host: "+host+"\n"
+    s = connection(host,port)
     
-        
+    token = "token="+token+"\n\n"
+    cType = "Content-Type: application/x-www-form-urlencoded\n"
+    cLength = "Content-Length: {}\n\n".format(len(token)-2)
+    s.send(request + reqHost + cType + cLength + token)
+    
+    solution = "solution=" + str(eval(getItem(s)))
+    token = "&token=" + token[6:]
+    cLength = "Content-Length: {}\n\n".format(len(solution + token)-2)
+    s = connection(host, port)
+    s.send(request + reqHost + cType + cLength + solution + token)
+    return getItem(s)
+
+def getFlag4(host, port):
+    request = "POST /getSecure HTTP/1.1\n"
+    reqHost = "Host: "+host+"\n"
+    s = connection(host,port)
+    s.send(request + reqHost + "Conncetion: keep-alive\n\n")
+    token = getItem(s)
+    s.close()
+    token = "&token="+token+"\n\n" 
+    
+    request = "POST /createAccount HTTP/1.1\n"
+    s = connection(host,port)
+    login = "username=chaim" +token
+    cType = "Content-Type: application/x-www-form-urlencoded\n"
+    cLength = "Content-Length: {}\n\n".format(len(login)-2)
+    userAgent = "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko\n"
+    accept = "Accept: text/html,application/xhtml+xml,appliaction/xml;q=0.9,*/*;q=0.8\n"
+    lang = "Accept-Language: en-US,en;q=0.5\n"
+    encoding = "Accept-Encoding: gzip, deflate\n"
+    s.send(request + reqHost + userAgent + accept + lang + encoding + cType + cLength + login)
+
+    password = ""
+    while True:
+        password = s.recv(1024)
+        if password:
+            password = password.split(' ')[-1].strip('\"').strip()
+        break
+    
+    request = "POST /login HTTP/1.1\n"
+    s = connection(host,port)
+    login = "username=chaim" +"&password="+ urllib.quote_plus(password)+token
+    cLength = "Content-Length: {}\n\n".format(len(login)-2)
+ 
+    print (request + reqHost + userAgent + accept + lang + encoding + cType + cLength + login)
+    s.send(request + reqHost + userAgent + accept + lang + encoding + cType + cLength + login)
+    
+    return getItem(s)
 
 def main():
     host = '54.209.150.110'
     port = 80
-    print("Activity 1: " + getFlag(host, port))
-    print getToken(host, port)
-    print("Activity 2: " + getFlag2(host, port, getToken(host, port)))
+    print "Activity 1 Flag = " + getFlag1(host, port)
+    print "Activity 2 Flag = " + getFlag2(host, port)
+    print "Activity 3 Flag = " + getFlag3(host, port) 
+    print "Activity 4 Flag = " + getFlag4(host, port)
 
 main()
